7e5db549fb03989c11f784b790a51931dd2a6a4e
renanrufino <renanrufino@gmail.com>
2021-05-21 00:18:34 -0300

    :package: Funcionalidade Contato

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 var hamburguer = document.querySelector(".hamburguer");
+var click = document.querySelector(".menu-link")
 
 hamburguer.addEventListener("click", function () {
     var sidebar = document.querySelector(".container").classList.toggle("show-menu");
+});
+
+click.addEventListener("click", function () {
+    var sidebar = document.querySelector(".container").classList.toggle("hide-menu");
 });
